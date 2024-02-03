004a2776882bed3db40f5f11dbd5111fa65e3c39
ENOSISBD\ashraful <ashraful.fuad@enosisbd.com>
2024-02-03 12:46:24 +0600

    Add allow null and blank

--- content_apis/models.py
+++ content_apis/models.py
@@ -1,33 +1,32 @@
 from django.db import models
 
 
-
 class Author(models.Model):
-    author_id = models.IntegerField()
-    username = models.CharField(max_length=100)
+    author_id = models.IntegerField(null=True)
+    username = models.CharField(max_length=100, null=True, blank=True)
 
 
 class Content(models.Model):
-    unique_id = models.IntegerField()
-    unique_uuid = models.CharField(max_length=100)
-    origin_unique_id = models.CharField(max_length=100)
-    creation_date= models.DateTimeField()
-    creation_timestamp = models.DateTimeField()
-    main_text = models.TextField()
-    token_count = models.IntegerField()
-    char_count = models.IntegerField()
-    tag_count = models.IntegerField()
-    origin_platform = models.CharField(max_length=100)
-    origin_url = models.CharField(max_length=1000)
-    like_count = models.IntegerField()
-    view_count = models.IntegerField()
-    comment_count = models.IntegerField()
+    unique_id = models.IntegerField(null=True)
+    unique_uuid = models.CharField(max_length=100, null=True, blank=True)
+    origin_unique_id = models.CharField(max_length=100, null=True, blank=True)
+    creation_date= models.DateTimeField(null=True, blank=True)
+    creation_timestamp = models.DateTimeField(null=True, blank=True)
+    main_text = models.TextField(null=True, blank=True)
+    token_count = models.IntegerField(null=True)
+    char_count = models.IntegerField(null=True)
+    tag_count = models.IntegerField(null=True)
+    origin_platform = models.CharField(max_length=100, null=True, blank=True)
+    origin_url = models.CharField(max_length=1000, null=True, blank=True)
+    like_count = models.IntegerField(null=True)
+    view_count = models.IntegerField(null=True)
+    comment_count = models.IntegerField(null=True)
     
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
 
 class MediaUrls(models.Model):
-    url = models.CharField(max_length=1000)
-    media_type = models.CharField(max_length=1000)
+    url = models.CharField(max_length=1000, null=True, blank=True)
+    media_type = models.CharField(max_length=1000, null=True, blank=True)
     
     content = models.ForeignKey(Content, on_delete=models.CASCADE)
