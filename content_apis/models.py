0759352be0695e51ac63e3e7cf55f09582667d8e
ENOSISBD\ashraful <ashraful.fuad@enosisbd.com>
2024-02-03 11:35:02 +0600

    Add content related models

--- content_apis/models.py
+++ content_apis/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Author(models.Model):
+    author_id = models.IntegerField()
+    username = models.CharField(max_length=100)
+
+
+class Content(models.Model):
+    unique_id = models.IntegerField()
+    unique_uuid = models.CharField(max_length=100)
+    origin_unique_id = models.CharField(max_length=100)
+    creation_date= models.DateTimeField()
+    creation_timestamp = models.DateTimeField()
+    main_text = models.TextField()
+    token_count = models.IntegerField()
+    char_count = models.IntegerField()
+    tag_count = models.IntegerField()
+    origin_platform = models.CharField(max_length=100)
+    origin_url = models.CharField(max_length=1000)
+    like_count = models.IntegerField()
+    view_count = models.IntegerField()
+    comment_count = models.IntegerField()
+    
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
+class MediaUrls(models.Model):
+    url = models.CharField(max_length=1000)
+    media_type = models.CharField(max_length=1000)
+    
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
