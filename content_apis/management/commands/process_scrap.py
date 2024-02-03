896d2976d148340d87eda0db64d81a1828491b56
ENOSISBD\ashraful <ashraful.fuad@enosisbd.com>
2024-02-03 20:25:46 +0600

    Add scrapped data in db

--- content_apis/management/commands/process_scrap.py
+++ content_apis/management/commands/process_scrap.py
@@ -4,35 +4,59 @@ from content_apis.models import UserInfo, VideoUrl, PhotoUrl
 from pydash import get
 
 from .scrap_data import SCRAPED_DATA
+import requests
+
+from content_apis.serializers import UserInfoSerializer
 
 class Command(BaseCommand):
     help = "Add scraped data"
 
     def handle(self, *args, **options):
-        for data in SCRAPED_DATA:
-            user_info = UserInfo(
-                user_id=data.get("user_id"),
-                username=data.get("username"),
-                full_name=data.get("full_name"),
-                profile_pic_url=data.get("profile_pic_url")
-            )
+        api_endpoint = 'https://3c9pj1-ip-202-84-41-237.tunnelmole.net/api/'
 
-            user_info.save()
+        response = requests.get(api_endpoint)
 
-            video_urls = data.get("video_url", [])
+        scrapped_data = response.json()
 
-            video_url_instances = [
-                VideoUrl(url=video_url, user=user_info)
-                for video_url in video_urls
-            ]
+        new_ids = [
+            data.get("user_id") for data in scrapped_data
+        ]
 
-            VideoUrl.objects.bulk_create(video_url_instances)
+        existing_user_infos = UserInfo.objects.filter(user_id__in=new_ids)
 
-            photo_urls = data.get("photo_url", [])
-        
-            photo_url_instances = [
-                PhotoUrl(url=photo_url, user=user_info)
-                for photo_url in photo_urls
-            ]
-        
-            PhotoUrl.objects.bulk_create(photo_url_instances)
+        serializer = UserInfoSerializer(existing_user_infos, many=True)
+
+        existing_user_ids = [
+            user_info.get("user_id") for user_info in serializer.data
+        ]
+
+
+        for data in scrapped_data:
+
+            if data.get("user_id") not in existing_user_ids:
+                user_info = UserInfo(
+                    user_id=data.get("user_id"),
+                    username=data.get("username"),
+                    full_name=data.get("full_name"),
+                    profile_pic_url=data.get("profile_pic_url")
+                )
+
+                user_info.save()
+
+                video_urls = data.get("video_url", [])
+
+                video_url_instances = [
+                    VideoUrl(url=video_url, user=user_info)
+                    for video_url in video_urls
+                ]
+
+                VideoUrl.objects.bulk_create(video_url_instances)
+
+                photo_urls = data.get("photo_url", [])
+            
+                photo_url_instances = [
+                    PhotoUrl(url=photo_url, user=user_info)
+                    for photo_url in photo_urls
+                ]
+            
+                PhotoUrl.objects.bulk_create(photo_url_instances)
